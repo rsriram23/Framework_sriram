0e89f4963176fced7de166a39887e19b652f0da8
pradeekumi <pradeekumi@gmail.com>
2026-03-16 15:52:00 +0530

    added explicit_wait and sleep

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,9 @@
 
 
 from selenium.webdriver.common.by import By
-
+from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Chrome,ChromeOptions
 
 
