7708f96b0143b619769e06949755c10e4e77b4c0
pradeekumi <pradeekumi@gmail.com>
2026-03-16 16:33:10 +0530

    removed sleep

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,6 @@
 
 
 from selenium.webdriver.common.by import By
-from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Chrome,ChromeOptions
